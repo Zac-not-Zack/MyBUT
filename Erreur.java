b32382fd1fdd01944869f46baa204ceb2697ef95
Zac Wong <78658974+Zac-not-Zack@users.noreply.github.com>
2022-03-26 21:58:36 +0100

    Add files via upload

---
+++ Erreur.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class Erreur extends AppCompatActivity {
+
+    private Intent output;
+    private String affichage;
+    private EditText output4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_erreur);
+
+        output=getIntent();
+        affichage= output.getStringExtra("Val1");
+        output4= findViewById(R.id.output4);
+        output4.setText(affichage);
+    }
+}
