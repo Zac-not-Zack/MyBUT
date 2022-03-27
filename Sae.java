142920ed4734f2b4bc3b9d16b105b4f7fc94f507
ADzac <danielzack786@gmail.com>
2022-03-27 22:29:26 +0200

    Create Sae.java

---
+++ Sae.java
@@ -0,0 +1,167 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class SAE extends AppCompatActivity {
+    private Intent output;
+    private Future<String> todo;
+    private String affichage;
+    private ArrayList<BUT> lf;
+    private ListView vl;
+    private ExecutorService exe;
+    private ArrayAdapter<BUT> aaf;
+    private List<String> tab = new ArrayList<String>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sae);
+
+        output = getIntent();
+        String s;
+        s = output.getStringExtra("Val1");
+        JSONArray jsonArray, sonArray;
+        lf = new ArrayList<>();
+        vl = findViewById(R.id.listeSAE);
+        try {
+            jsonArray = new JSONArray(s);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject json=  new JSONObject();
+                int id = jsonArray.getJSONObject(i).getInt("id");
+                String nom = jsonArray.getJSONObject(i).getString("nom");
+                String cours = jsonArray.getJSONObject(i).getString("cours");
+                String [] crs = cours.split(":");
+                crs[0] = crs[0] + " heures";
+                String tdirige = jsonArray.getJSONObject(i).getString("td");
+                String[] td = tdirige.split(":");
+                td[0] = td[0] + " heures";
+                String tpratique = jsonArray.getJSONObject(i).getString("tp");
+                String[] tp = tpratique.split(":");
+                tp[0] = tp[0] + " heures";
+                String projet = jsonArray.getJSONObject(i).getString("projet");
+                String[] pr = projet.split(":");
+                pr[0] = pr[0] + " heures";
+
+                tab.add("ID SAE " + ": " + id + "\n" + "Nom " + ": " + nom + "\n"
+                        + "Cours " + ": " + crs[0]  + "\n" + "TD " + ": " + td[0] + "\n"
+                        + "TP " + ": " + tp[0] + "\n" + "Projet " + ": " + pr[0] + "\n"  + "\n");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        for(int j = 0; j<tab.size();j++){
+            lf.add(new BUT(tab.get(j)));
+        }
+        //vl.setText(h);
+        aaf = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lf);
+        vl.setAdapter(aaf);
+        // vl.setOnItemClickListener(this);
+        registerForContextMenu(vl);
+        aaf.notifyDataSetChanged();
+        //titreFilm.setText("");
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+        contextMenu.setHeaderTitle("Actions");
+        contextMenu.add(Menu.NONE, view.getId(), 1, "Description");
+    }
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        String s;
+        URL u;
+        Intent action;
+        if(info.position==0) {
+            s = "http://infort.gautero.fr/index2022.php?action=get&obj=sae&idSae=2";
+            action = new Intent(this, MainActivity4.class);
+            try {
+                u = new URL(s);
+            } catch (MalformedURLException e) {
+                // Ce n'est qu'un exemple, pas de traitement propre de l'exception
+                e.printStackTrace();
+                u = null;
+            }
+            // On crée l'objet qui va gérer la thread
+            exe = Executors.newSingleThreadExecutor();
+            // On lance la thread
+            Future<String> todo = lireURL(u);
+            // On attend le résultat
+            try {
+                s = todo.get();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(s);
+            action.putExtra("Val1", s);
+            this.startActivity(action);
+            //lf.remove(info.position);
+            aaf.notifyDataSetChanged();
+        }
+        else {
+            action = new Intent(this, Erreur.class);
+            s="Pas d'information";
+            action.putExtra("Val1", s);
+            this.startActivity(action);
+            //lf.remove(info.position);
+            aaf.notifyDataSetChanged();
+        }
+
+        return true;
+    }
+
+
+    public Future<String> lireURL(URL u) {
+        return exe.submit(() -> {
+            URLConnection c;
+            String inputline;
+            StringBuilder codeHTML = new StringBuilder("");
+            try {
+                c = u.openConnection();
+                //temps maximun alloué pour se connecter
+                c.setConnectTimeout(60000);
+                //temps maximun alloué pour lire
+                c.setReadTimeout(60000);
+                //flux de lecture avec l'encodage des caractères UTF-8
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(c.getInputStream(), "UTF-8"));
+                while ((inputline = in.readLine()) != null) {
+                    //concaténation+retour à la ligne avec \n
+                    codeHTML.append(inputline + "\n");
+                }
+                //il faut bien fermer le flux de lecture
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return codeHTML.toString();
+        });
+    }
+}
