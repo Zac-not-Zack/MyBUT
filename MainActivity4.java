5bcbebf29d4f6dd3687cdb4b0f3900eaf2aeac57
ADzac <danielzack786@gmail.com>
2022-03-27 22:07:53 +0200

    Update MainActivity4.java

--- MainActivity4.java
+++ MainActivity4.java
@@ -2,24 +2,96 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MainActivity4 extends AppCompatActivity {
 
     private Intent output;
     private String affichage;
-    private EditText output2;
+    private TextView output3;
+    private ArrayAdapter<BUT> aaf;
+    private ExecutorService exe;
+    private Future<String> todo;
+    private List<String> tab = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
-        output=getIntent();
-        affichage= output.getStringExtra("Val1");
-        output2= findViewById(R.id.output2);
-        output2.setText(affichage);
+        output = getIntent();
+        String s;
+        s = output.getStringExtra("Val1");
+        JSONArray jsonArray, sonArray;
+        output3 = (TextView) findViewById(R.id.textView);
+        try {
+            jsonArray = new JSONArray(s);
+            sonArray = new JSONArray();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject json=  new JSONObject();
+                String desc = jsonArray.getJSONObject(i).getString("description");
+                output3.setText(desc);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Future<String> lireURL(URL u) {
+        return exe.submit(() -> {
+            URLConnection c;
+            String inputline;
+            StringBuilder codeHTML = new StringBuilder("");
+            try {
+                c = u.openConnection();
+                //temps maximun alloué pour se connecter
+                c.setConnectTimeout(60000);
+                //temps maximun alloué pour lire
+                c.setReadTimeout(60000);
+                //flux de lecture avec l'encodage des caractères UTF-8
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(c.getInputStream(), "UTF-8"));
+                while ((inputline = in.readLine()) != null) {
+                    //concaténation+retour à la ligne avec \n
+                    codeHTML.append(inputline + "\n");
+                }
+                //il faut bien fermer le flux de lecture
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return codeHTML.toString();
+        });
     }
 }
